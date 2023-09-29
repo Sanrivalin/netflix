820993912109178ff7e146bbc850bd4660fc538d
Sanrivalin <santiagorivasmolina@gmail.com>
2023-09-29 16:29:14 -0500

    September 29,2023 commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Main = () => {
+    const [movies, setMovies] = useState()
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Main
