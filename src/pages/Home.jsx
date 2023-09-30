52a3e90a565f5f03248fc1e648f8f0db4e219f51
Sanrivalin <santiagorivasmolina@gmail.com>
2023-09-30 17:22:58 -0500

    September 30,2023 commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Main from '../components/Main'
+import Row from '../components/Row'
+import requests from '../Requests'
 
 const Home = () => {
   return (
-    <div>
+    <>
+        <Main/>
+        <Row title='UpComing' fetchURL={requests.requestUpcoming}/>
+        <Row title='Popular' fetchURL={requests.requestPopular}/>
+        <Row title='Trending' fetchURL={requests.requestTrending}/>
+        <Row title='Top Rated' fetchURL={requests.requestTopRated}/>
+        <Row title='Horror' fetchURL={requests.requestHorror}/>
       
-    </div>
+    </>
   )
 }
 
