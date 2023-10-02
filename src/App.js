a0980dba0499434dc92e5c66c4a645f293881db9
Sanrivalin <santiagorivasmolina@gmail.com>
2023-10-01 23:14:51 -0500

    Octuber 1, 2023 commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 
 
 
 function App() {
   return (
     <>
-      <Navbar/>
+    <AuthContextProvider>
+    <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/account' element={<ProtectedRoute><Account /></ProtectedRoute> } />
       </Routes>
+    </AuthContextProvider>      
     </>
   );
 }
