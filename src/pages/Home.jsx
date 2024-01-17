47d40f090375884100543d250037b850c1d07d1d
Sanrivalin <santiagorivasmolina@gmail.com>
2024-01-16 23:39:36 -0500

    january 16, 2024 third commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <Row rowID='2' title='Popular' fetchURL={requests.requestPopular}/>
         <Row rowID='3' title='Trending' fetchURL={requests.requestTrending}/>
         <Row rowID='4' title='Top Rated' fetchURL={requests.requestTopRated}/>
-        <Row rowID='5' title='Horror' fetchURL={requests.requestHorror}/>
+        <Row rowID='5' title='Just added' fetchURL={requests.requestHorror}/>
       
     </>
   )
